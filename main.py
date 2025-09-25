59edace8b088b7bd8eb42413b59ffdf657671784
Globalfun32y439y54 <165802055+Globalfun32y439y54@users.noreply.github.com>
2025-09-25 15:34:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,60 @@
+#region VEXcode Generated Robot Configuration
+from vex import *
+import urandom
+import math
+
+# Brain should be defined by default
+brain=Brain()
+
+# Robot configuration code
+controller_1 = Controller(PRIMARY)
+DT_L1 = Motor(Ports.PORT11, GearSetting.RATIO_6_1, True)
+DT_L2 = Motor(Ports.PORT12, GearSetting.RATIO_6_1, True)
+DT_L3 = Motor(Ports.PORT13, GearSetting.RATIO_6_1, True)
+DT_R1 = Motor(Ports.PORT18, GearSetting.RATIO_6_1, False)
+DT_R2 = Motor(Ports.PORT19, GearSetting.RATIO_6_1, False)
+DT_R3 = Motor(Ports.PORT20, GearSetting.RATIO_6_1, False)
+Stage1Motor = Motor(Ports.PORT4, GearSetting.RATIO_6_1, False)
+Stage2Motor = Motor(Ports.PORT7, GearSetting.RATIO_6_1, False)
+Preload_Arm = DigitalOut(brain.three_wire_port.a)
+
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+# Make random actually random
+def initializeRandomSeed():
+    wait(100, MSEC)
+    random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
+    urandom.seed(int(random))
+      
+# Set random seed 
+initializeRandomSeed()
+
+
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening.
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
+print("\033[2J")
+
+#endregion VEXcode Generated Robot Configuration
+
+# ------------------------------------------
+# 
+# 	Project: PushbackDTCodePhilosophers
+#	Author: BrayDog2010
+#	Created: 9/10/25
+#	Description: VEXcode V5 Python Project For Team TKA 63544C
+# 
+# ------------------------------------------
+
 myVariable = 0
 DT_L_Velocity = 0
 DT_R_Velocity = 0
